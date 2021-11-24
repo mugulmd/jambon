9a2ec189059133fdb21adc83cd4fae0f5f951ea7
mugulmd <mugulmotion@gmail.com>
2021-11-24 16:17:29 +0100

    basic server architecture

---
+++ index.js
@@ -0,0 +1,24 @@
+/*
+	Server
+	Program entry point
+*/
+
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+	console.log('a user connected');
+	socket.on('disconnect', () => {
+		console.log('user disconnected');
+	});
+});
+
+server.listen(8000, () => {
+	console.log('listening on *:8000');
+});
