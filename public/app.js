d4128cf723199292321eec0d473f726a55d4b3e2
mugulmd <mugulmotion@gmail.com>
2021-11-29 09:55:36 +0100

    small improvements on sequencer

--- public/app.js
+++ public/app.js
@@ -23,18 +23,29 @@ let drums = new Tone.Players({
 
 let sequencer = document.getElementById("sequencer");
 
+let steps = [];
+
 for (let i = 0; i < 3; i++) {
 
 	let row = document.createElement("div");
 	row.classList.add("row");
 
+	steps.push([]);
+
 	for (let j = 0; j < 8; j++) {
-		let cell = document.createElement("button");
+		let cell = document.createElement("input");
+		cell.type = "checkbox";
+
+		steps[i].push(0);
+
 		cell.onclick = function() {
 			console.log(i + ", " + j);
-			Tone.Transport.scheduleRepeat((time) => {
-				drums.player(i).start(time);
-			}, {"4n": 2}, {"16n": j});
+			if(cell.checked) { 
+				steps[i][j] = Tone.Transport.scheduleRepeat((time) => { drums.player(i).start(time); }, {"4n": 2}, {"16n": j});
+			}
+			else {
+				Tone.Transport.clear(steps[i][j]);
+			}
 		};
 
 		cell.classList.add("cell");
@@ -47,11 +58,16 @@ for (let i = 0; i < 3; i++) {
 
 // Play-pause
 
-document.getElementById("buttonPlay").onclick = async function() {
-	await Tone.start();
-	console.log("audio is ready");
-
+document.getElementById("buttonPlay").onclick = function() {
 	console.log("play");
 	Tone.Transport.bpm.value = 60;
-	Tone.Transport.start();
+	Tone.Transport.toggle();
 };
+
+
+// Initialization
+
+window.onload = async function() {
+	await Tone.start();
+	console.log("audio is ready");
+}
